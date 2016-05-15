d6d4d7ad17089f700b78ecc2503bfef61b4a3117
Cecilia Sachetti <ceci.sachetti@gmail.com>
2016-05-15 00:14:29 -0300

    Drawing over the image

--- app/src/main/java/edu/unicen/project/dicomseg/activities/DicomViewActivity.java
+++ app/src/main/java/edu/unicen/project/dicomseg/activities/DicomViewActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Point;
 import android.os.Bundle;
 
@@ -31,6 +32,11 @@ public class DicomViewActivity extends Activity {
     private static final String TAG = "DicomViewActivity";
     private GestureDetector gestureDetector;
 
+    private Path mPath;
+    private Paint mPaint;
+    private float mX, mY;
+    private final float TOUCH_TOLERANCE = 4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,16 +47,16 @@ public class DicomViewActivity extends Activity {
         final String fileName = (String) getIntent().getSerializableExtra("fileName");
 
         final Integer imageNumber = (Integer) getIntent().getSerializableExtra("imageNumber");
-        Bitmap renderBitmap = DicomUtils.getFrame(imageNumber);
+        final Bitmap dicomFrame = DicomUtils.getFrame(imageNumber);
 
-        Bitmap mutableBitmap = renderBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        final Bitmap mutableBitmap = dicomFrame.copy(Bitmap.Config.ARGB_8888, true);
         final Canvas canvas = new Canvas(mutableBitmap);
 
         final ImageView imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setImageBitmap(mutableBitmap);
 
-        FloatingActionButton generalNote = (FloatingActionButton) findViewById(R.id.menu_general_note);
-        generalNote.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton generalNoteButton = (FloatingActionButton) findViewById(R.id.menu_general_note);
+        generalNoteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), GeneralNoteActivity.class);
@@ -60,8 +66,8 @@ public class DicomViewActivity extends Activity {
             }
         });
 
-        FloatingActionButton pointNote = (FloatingActionButton) findViewById(R.id.menu_point_note);
-        pointNote.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton pointNoteButton = (FloatingActionButton) findViewById(R.id.menu_point_note);
+        pointNoteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FloatingActionMenu menu = (FloatingActionMenu) findViewById(R.id.menu);
@@ -76,7 +82,7 @@ public class DicomViewActivity extends Activity {
                     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
                     paint.setColor(Color.rgb(51, 98, 178));
                     paint.setStyle(Paint.Style.FILL);
-                    // TODO: scale circle accordingly (to the image)
+                    // TODO: scale circle accordingly (to the image, which is automatically scaled to fit screen)
                     canvas.drawCircle(point.x, point.y, 8, paint);
                 }
 
@@ -87,13 +93,7 @@ public class DicomViewActivity extends Activity {
                     public boolean onTouch(View view, MotionEvent event) {
                         if (gestureDetector.onTouchEvent(event)) {
 
-                            final int index = event.getActionIndex();
-                            final float[] coords = new float[] { event.getX(index), event.getY(index) };
-                            Matrix matrix = new Matrix();
-                            imageView.getImageMatrix().invert(matrix);
-                            matrix.postTranslate(imageView.getScrollX(), imageView.getScrollY());
-                            matrix.mapPoints(coords);
-
+                            float[] coords = getCoordsForCanvas(event, imageView);
                             int x = (int) coords[0];
                             int y = (int) coords[1];
 
@@ -110,9 +110,92 @@ public class DicomViewActivity extends Activity {
                         }
                     }
                 });
+
+                // TODO: refresh screen to draw point notes after a new point note is created/updated
             }
         });
 
+        FloatingActionButton segmentImageButton = (FloatingActionButton) findViewById(R.id.menu_segment);
+        segmentImageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                FloatingActionMenu menu = (FloatingActionMenu) findViewById(R.id.menu);
+                menu.hideMenu(true);
+
+                mPath = new Path();
+
+                mPaint = new Paint();
+                mPaint.setAntiAlias(true);
+                mPaint.setDither(true);
+                mPaint.setColor(Color.rgb(51, 98, 178));
+                mPaint.setStyle(Paint.Style.STROKE);
+                mPaint.setStrokeJoin(Paint.Join.ROUND);
+                mPaint.setStrokeCap(Paint.Cap.ROUND);
+                // TODO: scale stroke according to the image size
+                mPaint.setStrokeWidth(8);
+
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent event) {
+
+                        float[] coords = getCoordsForCanvas(event, imageView);
+                        int x = (int) coords[0];
+                        int y = (int) coords[1];
+
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                                touch_start(x, y);
+                                view.invalidate();
+                                break;
+                            case MotionEvent.ACTION_MOVE:
+                                touch_move(x, y);
+                                view.invalidate();
+                                break;
+                            case MotionEvent.ACTION_UP:
+                                touch_up(canvas);
+                                view.invalidate();
+                                break;
+                        }
+                        return true;
+                    }
+                });
+            }
+        });
+    }
+
+    private void touch_start(int x, int y) {
+        mPath.reset();
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    private void touch_move(int x, int y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+            mX = x;
+            mY = y;
+        }
+    }
+
+    private void touch_up(Canvas mCanvas) {
+        mPath.lineTo(mX, mY);
+        mCanvas.drawPath(mPath, mPaint);
+        mPath.reset();
+    }
+
+    private float[] getCoordsForCanvas(MotionEvent event, ImageView imageView) {
+        final int index = event.getActionIndex();
+        final float[] coords = new float[] { event.getX(index), event.getY(index) };
+        Matrix matrix = new Matrix();
+        imageView.getImageMatrix().invert(matrix);
+        matrix.postTranslate(imageView.getScrollX(), imageView.getScrollY());
+        matrix.mapPoints(coords);
+
+        return coords;
     }
 
 }
